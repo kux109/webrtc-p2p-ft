7a56cdeffab158c55123b190ba1cf12456bd728b
Kushagra Srivastva <kushagra147@gmail.com>
2024-11-10 23:56:29 -0500

    Stop tracking node_modules

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('candidate', (data) => {
+    socket.broadcast.emit('candidate', data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
